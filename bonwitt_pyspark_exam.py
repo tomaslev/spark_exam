465563dda3522079371ecc0140e9b0ff7965c643
tomaslev <bonwitt@gmail.com>
2026-03-23 10:34:19 +0000

    updated exam file + notebook for testing

--- bonwitt_pyspark_exam.py
+++ bonwitt_pyspark_exam.py
@@ -1,8 +1,10 @@
 from pyspark.sql import SparkSession
 from pyspark import SparkContext
 from pyspark.conf import SparkConf
+from pyspark.sql.functions import *
 import urllib.request as ur
 
+#Q.1. Using the urlretrieve function from the urllib.request module, write a download_file function to download a filename from the previously mentioned global address. Apply this function to the files we want to download.
 def file_dl(filename):
     url = "https://assets-datascientest.s3.eu-west-1.amazonaws.com/" + filename
     ur.urlretrieve(url, filename)
@@ -10,6 +12,7 @@ def file_dl(filename):
 file_dl("gps_app.csv")
 file_dl("gps_user.csv")
 
+conf = SparkConf()
 sc = SparkContext.getOrCreate(conf=conf)
 
 # Define a SparkSession
@@ -19,3 +22,24 @@ spark = SparkSession \
     .appName("pyspark_exam") \
     .getOrCreate()
 spark
+
+raw_app = spark.read.option("header", True)\
+                    .option("inferSchema", True)\
+                    .option("escape", "\"")\
+                    .csv("gps_app.csv")
+
+raw_user = spark.read.option("header", True)\
+                     .option("inferSchema", True)\
+                     .option("escape", "\"")\
+                     .csv("gps_user.csv")
+                     
+#Q.2. In an initial preprocessing step, rename all columns by replacing spaces with underscores and converting uppercase letters to lowercase.
+for col in raw_app.columns:
+   raw_app = raw_app.withColumnRenamed(col, col.replace(" ", "_").lower())
+   
+for col in raw_user.columns:
+   raw_user = raw_user.withColumnRenamed(col, col.replace(" ", "_").lower())
+   
+#Q.3.1 Replace missing values in the rating column with the mean or median. Justify your choice.
+#checking the skewdness of rating to determine whenther to use median or mean
+raw_app.filter(~isnan("rating")).select(skewness("rating")).show()
